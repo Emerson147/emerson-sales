e3da2931ac7ef2db3178403776d4821ac26b4c2e
Emerson <75981434@continental.edu.pe>
2023-03-13 07:54:44 -0500

    Initial commit

---
+++ src/main/java/com/emersoncode/EmersonSalesApplication.java
@@ -0,0 +1,13 @@
+package com.emersoncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmersonSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmersonSalesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emersoncode/EmersonSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emersoncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmersonSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
